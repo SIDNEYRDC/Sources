7ef6140c8811422d155f05eeab69d67b83bef6c4
SIDNEYRDC <sydney_rdc@hotmail.com>
2015-05-12 20:18:19 -0300

    First commit

---
+++ make/utils.c
@@ -0,0 +1,9 @@
+#include "utils.hpp"
+
+using namespace std;
+
+// Return the maximum between two floats
+float max_float(float x, float y) {
+	if(x > y) return x;
+	return y;
+}
---
+++ make/utils.hpp
@@ -0,0 +1,17 @@
+/**********************************************************************
+*	Tools for default C\C++ Programming
+*	Written by Sidney RDC, 2014.
+***********************************************************************/
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <vector>
+
+// Return the maximum between two floats
+float max_float(float x, float y);
+
+#endif
